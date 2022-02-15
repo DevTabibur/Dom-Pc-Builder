7a759b1578610a190b2f1003e4939cf5bf6d70f3
DevTabibur <tobiburrohman2@gmail.com>
2022-02-15 21:29:03 +0600

    all done with advance way

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
 // @ts-nocheck
-/*
-document.getElementById("sixteenGB").addEventListener("click", function (){
-    const memoryCost = document.getElementById("memory-cost");
-    const memoryCostValue = memoryCost.innerText;
-    memoryCost.innerText = 200;
-    // console.log(memoryCostValue);
-
-    // get the whole cost 
-    const bestPrice = document.getElementById("best-price").innerText;
-    const storageCost = document.getElementById("storage-cost").innerText;
-    const deliveryCost = document.getElementById("delivery-cost").innerText;
-    // addition the values
-    const totalPrice = parseFloat(bestPrice) + parseFloat(storageCost) + parseFloat(deliveryCost) + parseFloat(memoryCostValue);
-    console.log(totalPrice);
-})
-*/
 
 // handle memory eightGB
 document.getElementById("eightGB").addEventListener("click", function (){
-    var memoryCost = document.getElementById("memory-cost");
-    memoryCost.innerText = 100;
-
-    // get all the id where the memory cost should be updated
-    const bestPrice = document.getElementById("best-price").innerText;
-    const storageCost = document.getElementById("storage-cost").innerText;
-    const deliveryCost = document.getElementById("delivery-cost").innerText;
-    
-    let totalPrice = document.getElementById("total-price");
-
-    totalPrice.innerText = parseInt(bestPrice) + parseInt(storageCost) + parseInt(deliveryCost) + parseInt(memoryCost.innerText);
-    // console.log(bestPrice, storageCost, deliveryCost)
+    updateMemoryProduct(100);
 })
-
 // handle memory sixteenGB
 document.getElementById("sixteenGB").addEventListener("click", function (){
+    updateMemoryProduct(200);
+})
+
+// function for handle memory product
+function updateMemoryProduct(price){
     var memoryCost = document.getElementById("memory-cost");
-    memoryCost.innerText = 200;
+    memoryCost.innerText = price;
 
     // get all the id where the memory cost should be updated
     const bestPrice = document.getElementById("best-price").innerText;
@@ -45,82 +22,40 @@ document.getElementById("sixteenGB").addEventListener("click", function (){
     let totalPrice = document.getElementById("total-price");
 
     totalPrice.innerText = parseInt(bestPrice) + parseInt(storageCost) + parseInt(deliveryCost) + parseInt(memoryCost.innerText);
-    // console.log(bestPrice, storageCost, deliveryCost)
-})
+};
 
-// handle storage ssd1
-// document.getElementById("ssd1").addEventListener("click", function (){
-//     const storageCost = document.getElementById("storage-cost");
-//     storageCost.innerText = 200;
+// function for handle ssd product
+function updateStorageProduct(price){
+    const storageCost = document.getElementById("storage-cost");
+    storageCost.innerText = price;
 
-//     // get all the id where the memory cost should be updated
-//     const bestPrice = document.getElementById("best-price").innerText;
-//     const storageCost = document.getElementById("storage-cost").innerText;
-//     const deliveryCost = document.getElementById("delivery-cost").innerText;
-    
-//     let totalPrice = document.getElementById("total-price");
+    // get all values
+    const bestPrice = document.getElementById("best-price").innerText;
+    const memoryCost = document.getElementById("memory-cost").innerText;
+    const deliveryCost = document.getElementById("delivery-cost").innerText;
+    const totalPrice = document.getElementById("total-price");
 
-//     totalPrice.innerText = parseInt(bestPrice) + parseInt(storageCost) + parseInt(deliveryCost) + parseInt(memoryCost.innerText);
-// })
-
-
+    // set all values
+    totalPrice.innerText = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost.innerText) + parseInt(deliveryCost);
+}
 
 // handle storage ssd1
 document.getElementById("ssd1").addEventListener("click", function (){
-    const storageCost = document.getElementById("storage-cost");
-    storageCost.innerText = 200;
-
-    // get all values
-    const bestPrice = document.getElementById("best-price").innerText;
-    const memoryCost = document.getElementById("memory-cost").innerText;
-    const deliveryCost = document.getElementById("delivery-cost").innerText;
-    const totalPrice = document.getElementById("total-price");
-
-    // set all values
-    totalPrice.innerText = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost.innerText) + parseInt(deliveryCost);
-
-    // console.log(parseInt(bestPrice), parseInt(memoryCost) , parseInt(storageCost.innerText) , parseInt(deliveryCost));
+    updateStorageProduct(200);
 })
-
 // handle storage ssd2
 document.getElementById("ssd2").addEventListener("click", function (){
-    const storageCost = document.getElementById("storage-cost");
-    storageCost.innerText = 400;
-
-    // get all values
-    const bestPrice = document.getElementById("best-price").innerText;
-    const memoryCost = document.getElementById("memory-cost").innerText;
-    const deliveryCost = document.getElementById("delivery-cost").innerText;
-    const totalPrice = document.getElementById("total-price");
-
-    // set all values
-    totalPrice.innerText = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost.innerText) + parseInt(deliveryCost);
-
-    // console.log(parseInt(bestPrice), parseInt(memoryCost) , parseInt(storageCost.innerText) , parseInt(deliveryCost));
+    updateStorageProduct(400);
 })
-
-// handle storage ssd3
+// handle storage ssd1
 document.getElementById("ssd3").addEventListener("click", function (){
-    const storageCost = document.getElementById("storage-cost");
-    storageCost.innerText = 600;
-
-    // get all values
-    const bestPrice = document.getElementById("best-price").innerText;
-    const memoryCost = document.getElementById("memory-cost").innerText;
-    const deliveryCost = document.getElementById("delivery-cost").innerText;
-    const totalPrice = document.getElementById("total-price");
-
-    // set all values
-    totalPrice.innerText = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost.innerText) + parseInt(deliveryCost);
-
-    // console.log(parseInt(bestPrice), parseInt(memoryCost) , parseInt(storageCost.innerText) , parseInt(deliveryCost));
+    updateStorageProduct(600);
 })
 
-
-// handle free delivery
-document.getElementById("free-delivery").addEventListener("click", function (){
+// function for handle delivery
+function updateDeliveryProduct(price){
     const deliveryCost = document.getElementById("delivery-cost");
-    deliveryCost.innerText = 0;
+    deliveryCost.innerText = price;
 
     // get all values
     const bestPrice = document.getElementById("best-price").innerText;
@@ -130,35 +65,22 @@ document.getElementById("free-delivery").addEventListener("click", function (){
 
     // set all the values
     totalPrice.innerText = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost.innerText);
+}
 
-    console.log(parseInt(bestPrice) , parseInt(memoryCost) , parseInt(storageCost) , parseInt(deliveryCost.innerText))
-
+// handle free delivery
+document.getElementById("free-delivery").addEventListener("click", function (){
+    updateDeliveryProduct(0);
 })
 
 // handle paid delivery
 document.getElementById("paid-delivery").addEventListener("click", function (){
-    const deliveryCost = document.getElementById("delivery-cost");
-    deliveryCost.innerText = 20;
-
-    // get all values
-    const bestPrice = document.getElementById("best-price").innerText;
-    const memoryCost = document.getElementById("memory-cost").innerText;
-    const storageCost = document.getElementById("storage-cost").innerText;
-    const totalPrice = document.getElementById("total-price");
-
-    // set all the values
-    totalPrice.innerText = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost.innerText);
-
-    console.log(parseInt(bestPrice) , parseInt(memoryCost) , parseInt(storageCost) , parseInt(deliveryCost.innerText))
-
+    updateDeliveryProduct(20);
 });
 
-
 // handle promo code
+const fakeCode = "pHero";
 document.getElementById("apply-btn").addEventListener("click", function (){
     const code = document.getElementById("promo-input").value;
-
-    const fakeCode = "pHero";
     if (code === fakeCode){
         const totalPrice = document.getElementById("total-price");
         let totalPriceValue = parseFloat(totalPrice.innerText) ;
